5ddb2358cb42c895843814963adce8889afd196f
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-23 00:08:11 +0200

    SPA project - React Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                {/* <li><a href="/">Start</a></li>
+                <li><a href="/news">Aktualności</a></li>
+                <li><a href="/contact">Kontakt</a></li> */}
+                <li>
+                  <Link to="/">Start</Link>
+                </li>
+                <li>
+                  <Link to="/news">Aktualności</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Kontakt</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>Strona - witaj</section>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
